1824169b94267e8eb073b1dde52df55e7800e64d
Jim McGee <jmcgee@jims-m1-mbp.lan>
2022-02-18 00:30:06 -0700

    forms

--- src/theme/Playground/index.js
+++ src/theme/Playground/index.js
@@ -38,7 +38,7 @@ function ResultWithHeader() {
 function ThemedLiveEditor() {
   const isBrowser = useIsBrowser();
   return (
-    <div style={{ overflowY: "auto", maxHeight: 500 }}>
+    <div style={{ overflowY: "auto", maxHeight: 450, width: "60%" }}>
       <LiveEditor
         // We force remount the editor on hydration,
         // otherwise dark prism theme is not applied
@@ -70,6 +70,193 @@ function ComponentWithRefresh({ children }) {
   );
 }
 
+const SubmitContext = React.createContext(null);
+
+function useSubmit() {
+  return React.useContext(SubmitContext);
+}
+
+function SubmitProvider({ children }) {
+  const [result, setResult] = React.useState(null);
+
+  const apiSubmit = React.useCallback((data) => {
+    setResult(null);
+    return new Promise((resolve) =>
+      setTimeout(() => {
+        setResult(data);
+        resolve();
+      }, 400)
+    );
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <SubmitContext.Provider value={apiSubmit}>
+          {children}
+        </SubmitContext.Provider>
+      </div>
+      {result && (
+        <>
+          <hr />
+          <div
+            style={{
+              padding: 8,
+              background: "tomato",
+              color: "white",
+              borderRadius: 8,
+            }}
+          >
+            Received by API: {JSON.stringify(result)}
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+const users = {
+  1: {
+    id: 1,
+    name: "Jim X. Magoo",
+    age: "59",
+    favoriteColor: "goldenrod",
+    isHungry: true,
+    isThirsty: false,
+  },
+  2: {
+    id: 2,
+    name: "Lizzy Gorbmacher-Drizzle",
+    age: "22",
+    favoriteColor: "tomato",
+    isHungry: false,
+    isThirsty: true,
+  },
+};
+
+function apiFetchUserDataById(id) {
+  return new Promise((resolve) => setTimeout(() => resolve(users[id], 500)));
+}
+
+const steps = [Step1, Step2, Step3];
+function UserForm({ userData }) {
+  const [tab, setTab] = React.useState(0);
+  const [formState, setFormState] = React.useState(
+    userData || {
+      name: null,
+      age: 14,
+      favoriteColor: null,
+      isHungry: false,
+      isThirsty: false,
+    }
+  );
+
+  function handleChange(update) {
+    setFormState((oldFormState) => ({ ...oldFormState, ...update }));
+  }
+
+  const apiSubmit = useSubmit(); // mocked function for this demo
+  async function handleSubmit(e) {
+    e.preventDefault();
+    await apiSubmit(formState);
+  }
+
+  const Step = steps[tab];
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        {steps.map((s, i) => (
+          <button
+            type="button"
+            onClick={() => setTab(i)}
+            style={{
+              margin: 2,
+              backgroundColor: i === tab ? "white" : "lightgray",
+              fontWeight: i === tab ? "bold" : null,
+            }}
+          >
+            {i + 1}
+          </button>
+        ))}
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </div>
+      <hr />
+      <Step value={formState} onChange={handleChange} />
+    </form>
+  );
+}
+
+function Step1({ value, onChange }) {
+  return (
+    <div>
+      <div>
+        Name:{" "}
+        <input
+          type="text"
+          value={value.name}
+          onChange={(e) => onChange({ name: e.target.value })}
+        />
+      </div>
+      <div>
+        Age:{" "}
+        <input
+          type="number"
+          value={value.age}
+          onChange={(e) => onChange({ name: e.target.age })}
+        />
+      </div>
+    </div>
+  );
+}
+
+function Step2({ value, onChange }) {
+  return (
+    <div>
+      <div>
+        Favorite Color:{" "}
+        <input
+          type="text"
+          value={value.favoriteColor}
+          onChange={(e) => onChange({ favoriteColor: e.target.value })}
+        />
+        <div
+          style={{
+            width: 100,
+            height: 100,
+            backgroundColor: value.favoriteColor,
+            margin: 4,
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+function Step3({ value, onChange }) {
+  return (
+    <div>
+      <div>
+        Are you hungry?{" "}
+        <input
+          type="checkbox"
+          checked={value.isHungry}
+          onChange={(e) => onChange({ isHungry: e.target.checked })}
+        />
+      </div>
+      <div>
+        Are you thirsty?{" "}
+        <input
+          type="checkbox"
+          checked={value.isThirsty}
+          onChange={(e) => onChange({ isThirsty: e.target.checked })}
+        />
+      </div>
+    </div>
+  );
+}
+
 export default function Playground({
   children,
   transformCode,
@@ -84,7 +271,14 @@ export default function Playground({
         transformCode={transformCode || ((code) => `${code}`)}
         theme={prismTheme}
         noInline={true}
-        scope={{ ...scope, ComponentWithRefresh }}
+        scope={{
+          ...scope,
+          ComponentWithRefresh,
+          useSubmit,
+          SubmitProvider,
+          UserForm,
+          apiFetchUserDataById,
+        }}
         {...props}
       >
         <ThemedLiveEditor />
